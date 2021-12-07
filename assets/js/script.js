7fa9992ea6656644777eb4c8d082fe0dc6c6983c
Waeoo <davin50@hotmail.com>
2021-12-07 20:38:17 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,35 +5,33 @@ document.addEventListener("DOMContentLoaded", function() {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
-            }
-            else {
+            } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
-                runGame(gameType);    
+                runGame(gameType);
             }
-        })
+        });
     }
 
     runGame("addition");
-})
 
+});
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
+ function runGame(gameType) {
 
-function runGame(gameType) {
-
-    // Creates two random numbers between 1 to 25
-    let num1 = Math.floor.apply(Math.random() * 25 +1);
-    let num2 = Math.floor.apply(Math.random() * 25 +1);
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else {
-        alert(`Unknown game type: ${gameType}`)
+        alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
 }
 
 function checkAnswer() {
